01d751af37568650db1d6e36233ff4c5d19d92d9
Matteo Gazzato <matteo@moku.io>
2018-07-23 23:33:53 +0200

    Repository initialization

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '11.0'
+use_frameworks!
+
+target 'Telephone Directory iOS' do
+  pod 'RealmSwift'
+end
---
+++ Telephone Directory iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Telephone Directory iOS
+//
+//  Created by Matteo Gazzato on 23/07/18.
+//  Copyright © 2018 Matteo Gazzato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
