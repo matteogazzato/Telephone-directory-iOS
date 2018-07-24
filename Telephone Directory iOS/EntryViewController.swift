4199b181ceb15131fa3b2ddb086425d21013e621
Matteo Gazzato <matteo@moku.io>
2018-07-25 01:21:12 +0200

    Adds navigation and validation fields logic for EntryViewController

--- Telephone Directory iOS/EntryViewController.swift
+++ Telephone Directory iOS/EntryViewController.swift
@@ -9,27 +9,74 @@
 import UIKit
 
 class EntryViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  // MARK: Outlets
+  @IBOutlet weak var firstNameTextField: UITextField!
+  @IBOutlet weak var lastNameTextField: UITextField!
+  @IBOutlet weak var phoneNumberTextField: UITextField!
+  
+  // MARK: Var and Constants
+  public var editMode = false
+  
+  // MARK: UI Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Do any additional setup after loading the view.
+    
+    if !editMode {
+      let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(goToHomeWithoutSaving))
+      self.navigationItem.leftBarButtonItem = cancelButton
+    } 
+    let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveAndGoToHome))
+    self.navigationItem.rightBarButtonItem = saveButton
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Navigation
+  @objc func goToHomeWithoutSaving() {
+    self.dismiss(animated: true, completion: nil)
+  }
+  
+  @objc func saveAndGoToHome() {
+    // TODO: Add operations to save/add new entry
+    // Check First Name and Last Name
+    var validation = true
+    validation = validation && validateFirstLastName()
+    validation = validation && validatePhoneNumber()
+    
+    
+    
+    self.dismiss(animated: true, completion: nil)
+  }
+  
+  // MARK: Fields Validation
+  private func validateFirstLastName() -> Bool {
+    guard let firstName = firstNameTextField.text else {
+      return false
     }
-    */
-
+    guard let lastName = lastNameTextField.text else {
+      return false
+    }
+    return firstName.count != 0 && lastName.count != 0
+  }
+  
+  private func validatePhoneNumber() -> Bool {
+    guard let phoneNumber = phoneNumberTextField.text else {
+      return false
+    }
+    // "+" followed by a nonempty group of digits, a space, a nonempty group of digits, a space, a group of digits with at least 6 digits
+    let phoneNumberPattern = "^\\+[0-9]{2} [0-9]{2} [0-9]{6,}$"
+    if let phoneNumberRegex = try? NSRegularExpression(pattern: phoneNumberPattern, options: []) {
+      let matches = phoneNumberRegex.numberOfMatches(in: phoneNumber, options: [], range: NSMakeRange(0, phoneNumber.count))
+      if matches > 0 {
+        return true
+        }
+      }
+    return false
+  }
 }
--- Telephone Directory iOS/HomeViewController.swift
+++ Telephone Directory iOS/HomeViewController.swift
@@ -10,20 +10,26 @@ import UIKit
 
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
   
+  // MARK: Outlets
   @IBOutlet weak var searchResultsTableView: UITableView!
   
+  // MARK: Var and Constants
+  static let EditEntrySegue = "EditEntrySegueIdentifier"
+  static let AddNewEntrySegue = "AddNewEntrySegueIdentifier"
+  
+  // MARK: UI Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
     
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
+  
   
   // MARK: UITabelView Delegate Methods
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -37,6 +43,28 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return 0
   }
-    
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if let selectedCell = tableView.cellForRow(at: indexPath) {
+      performSegue(withIdentifier: HomeViewController.EditEntrySegue, sender: selectedCell)
+    }
+  }
+  
+  // MARK: Navigation
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let entryNavigationViewController = segue.destination as! UINavigationController
+    let entryViewController = entryNavigationViewController.viewControllers.first as! EntryViewController
+    switch segue.identifier! {
+    case HomeViewController.AddNewEntrySegue:
+      entryViewController.editMode = false
+    case HomeViewController.EditEntrySegue:
+      entryViewController.editMode = true
+    default:
+      return
+    }
+  
+  }
+  
 }
 
